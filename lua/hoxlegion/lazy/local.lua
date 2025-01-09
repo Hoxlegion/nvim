71e6049da04d9d4feb596a19059896184d25a1ab
Hoxlegion <hoxlegion@gmail.com>
2025-01-09 18:15:08 +0100

    Added go langserver and autoclose brackets(local)

--- lua/hoxlegion/lazy/local.lua
+++ lua/hoxlegion/lazy/local.lua
@@ -12,28 +12,6 @@ local local_plugins = {
             vim.keymap.set("n", "[[r", function() rfc.show_next() end)
         end
     },
- --[[   {
-        "harpoon",
-        dir = "~/personal/harpoon",
-        config = function()
-            local harpoon = require("harpoon")
-
-            harpoon:setup()
-
-            vim.keymap.set("n", "<leader>A", function() harpoon:list():prepend() end)
-            vim.keymap.set("n", "<leader>a", function() harpoon:list():add() end)
-            vim.keymap.set("n", "<C-e>", function() harpoon.ui:toggle_quick_menu(harpoon:list()) end)
-
-            vim.keymap.set("n", "<C-h>", function() harpoon:list():select(1) end)
-            vim.keymap.set("n", "<C-t>", function() harpoon:list():select(2) end)
-            vim.keymap.set("n", "<C-n>", function() harpoon:list():select(3) end)
-            vim.keymap.set("n", "<C-s>", function() harpoon:list():select(4) end)
-            vim.keymap.set("n", "<leader><C-h>", function() harpoon:list():replace_at(1) end)
-            vim.keymap.set("n", "<leader><C-t>", function() harpoon:list():replace_at(2) end)
-            vim.keymap.set("n", "<leader><C-n>", function() harpoon:list():replace_at(3) end)
-            vim.keymap.set("n", "<leader><C-s>", function() harpoon:list():replace_at(4) end)
-        end
-    }, ]]--
     {
         "vim-apm", dir = "~/personal/vim-apm",
         config = function()
@@ -43,6 +21,13 @@ local local_plugins = {
             apm:setup({})
             vim.keymap.set("n", "<leader>apm", function() apm:toggle_monitor() end)
             --]]
+        end
+    },
+    {
+        "autoclose", dir = "~/personal/autoclose",
+        config = function()
+            local atc = require("autoclose")
+            atc.setup()
         end
     },
 }
